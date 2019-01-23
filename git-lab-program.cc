f011bbee22c35ade3d3a6d91759419d8678a12ec
Emma Rice <er512317@ohio.edu>
2019-01-22 19:05:12 -0500

    initial commit for file

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Emma Rice
+ *        Date: January 22, 2019
+ * Description: gives output "Hello Git!!"
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
